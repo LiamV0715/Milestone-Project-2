f1c483f79d1d57f4fe5d9d06b678182e678835a4
Andi Weber <adtweber@gmail.com>
2024-06-29 12:04:50 -0400

    created backend skeleton

--- server.js
+++ server.js
@@ -1,4 +1,22 @@
+// DEPENDENCIES
+const express = require('express')
+const app = express()
+
+// CONFIGURATION / MIDDLEWARE
+require('dotenv').config()
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+// ROOT
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'Welcome to the Pets App'
+    })
+})
+
+// CONTROLLERS  
+
 // LISTEN
 app.listen(process.env.PORT, () => {
-    console.log(`It worked!!: ${process.env.PORT}`)
+    console.log("Pets are cool")
 })
