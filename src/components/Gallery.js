71c0fd282b8c9512fa65f1ff333a6f42742c6092
Liam Vermette <Liam.vermette@gmail.com>
2024-07-06 13:20:32 -0400

    gallery api branch created

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-export default function Gallery() {
+
+const Gallery = () => {
+  const [names, setNames] = useState([]);
+
+  useEffect(() => {
+    fetch("postgres://localhost:5432/pets")
+      .then((response) => response.json())
+      .then((data) => setNames(data))
+      console.log(names)
+      .catch((error) => console.error("Error fetching names:", error));
+  }, []);
+
   return (
-    <main>
-      <h2>This is the pet gallery!</h2>
-    </main>
+    <div>
+      <h1>Name List</h1>
+      <ul>
+        {names.map((name, index) => (
+          <li key={index}>{name.name}</li>
+        ))}
+      </ul>
+    </div>
   );
-}
+};
 
+export default Gallery;
